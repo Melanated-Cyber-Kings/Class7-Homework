67f13b642edf0d9afacf91b51974e3fef331f796
Wasee <229995823+waseeconsulting-git@users.noreply.github.com>
2025-10-07 22:42:00 +0200

    homework BAM 1

---
+++ Week 1/BAM/BAM 1.2/ec2_script_2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install and start web server
+dnf update -y
+dnf install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+# Variable for the URL
+BASE_URL="http://169.254.169.254/latest"
+
+# Get token for metadata requests
+TOKEN=$(curl -X PUT "$BASE_URL/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 3600" -s)
+
+# Collect instance info and save to variables
+LOCAL_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "$BASE_URL/meta-data/local-ipv4")
+AZ=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "$BASE_URL/meta-data/placement/availability-zone")
+HOST_NAME=$(hostname -f)
+
+# Create simple webpage and save to /var/www/html/index.html
+# EOF marks where the HTML starts and stops basically 
+cat << EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>EC2 Be a man Challenge 1.1</title>
+</head>
+
+<body>
+    <h1>"I found my wife on a party yacht in Martinique! Her name is Alexia!"</h1>
+
+    <img src="https://github.com/Melanated-Cyber-Kings/Class7-Homework/blob/Van-Ngila-Homework-Branch/African/img.JPG">
+
+    
+    <h2>Instance Details</h2>
+    <p><strong>Hostname:</strong> ${HOST_NAME}</p>
+    <p><strong>Private IP:</strong> ${LOCAL_IP}</p>
+    <p><strong>Availability Zone:</strong> ${AZ}</p>
+</body>
+</html>
+EOF
